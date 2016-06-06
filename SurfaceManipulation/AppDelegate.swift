7c830477395357a48f5870c58279ae1f6b1cc1bd
Matt <7395051@gmail.com>
2016-06-06 06:15:38 -0500

    Initial Commit

---
+++ SurfaceManipulation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SurfaceManipulation
+//
+//  Created by Matthew Dillard on 6/6/16.
+//  Copyright © 2016 Matthew Dillard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SurfaceManipulation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SurfaceManipulation
+//
+//  Created by Matthew Dillard on 6/6/16.
+//  Copyright © 2016 Matthew Dillard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
